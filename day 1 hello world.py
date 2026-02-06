f9f576c2f45c8978a3e5cad763183bb9c7c34be4
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-02-06 07:50:11 +0530

    Add files via upload

---
+++ day 1 hello world.py
@@ -0,0 +1,5 @@
+print("hello world")
+print("welcome to python programming")
+print("this is bharadwaj writing his initial code in python")
+x= "pursuing bachelor of technology in information technology"
+print(x)
