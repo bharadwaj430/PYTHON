c10d992f9e3aafddb9a9cd084b2b020ed2a6681e
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-06 21:26:34 +0530

    Added formatting

---
+++ day 3/formatting.py
@@ -0,0 +1,6 @@
+name = "Bharadwaj"
+age = 19
+current_position = "B.tech student"
+print (name, age, current_position)  #printing multiple values
+print(name, age, current_position, sep="|")  #seperator
+print("My name is {} and I am {} years old and I am a {}".format(name, age, current_position)) #using format method
