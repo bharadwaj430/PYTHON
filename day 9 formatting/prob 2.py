46d4fd384a8ab8622d28452765836e26e00b1456
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-12 22:43:05 +0530

    Updated feature

--- day 9 formatting/prob 2.py
+++ day 9 formatting/prob 2.py
@@ -4,4 +4,4 @@ print(first_name + " " + last_name) #printing full name
 y = 20
 z= 9
 x =2
-print("The product of", y, "and", z, "adding x is", y*z+x) #printing product of two numbers and adding another number to it
+print("The product of", y, "and", z, "adding x = 2 is", y*z+x) #printing product of two numbers and adding another number to it
