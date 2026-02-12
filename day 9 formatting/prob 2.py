b1a3b924aadda4ed8cefee6f3bc068ce0f795db8
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-12 22:31:22 +0530

    Added string concatenation

---
+++ day 9 formatting/prob 2.py
@@ -0,0 +1,7 @@
+first_name = "Bharadwaj"
+last_name = "Marri"
+print(first_name + " " + last_name) #printing full name
+y = 20
+z= 9
+x =2
+print("The product of", y, "and", z, "adding x is", y*z+x) #printing product of two numbers and adding another number to it
