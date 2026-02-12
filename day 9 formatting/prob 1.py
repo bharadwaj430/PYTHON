0e61a37647b423884ade1f6626bdf85ec964a23d
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-12 22:22:52 +0530

    Added basic print formatting

---
+++ day 9 formatting/prob 1.py
@@ -0,0 +1,6 @@
+print("Hello, everyone" , end = " ") #print text
+n= "Bharadwaj"  #print by declaring a variable and assign a string
+print(n)
+age = 20 #printing age
+print(age)
+print("This is", n, "and I am", age, "years old.") #printing by comma seperation
