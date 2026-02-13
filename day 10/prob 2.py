7dc5ee82a7007a8379acf6006fe75b0b17772d28
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-13 19:54:04 +0530

    Added same and multiple values to multiple var

---
+++ day 10/prob 2.py
@@ -0,0 +1,9 @@
+#assigning multiple values to variables
+x, y, z = 1, 2, 3
+print(x, y, z)
+#assigning the same value to multiple variables
+a = b = c = 4 
+print(a, b, c)
+print(a)
+print(b)
+print(c)
