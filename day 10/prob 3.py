f0a9441732c8691a178dcdab54c35433b526df85
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-13 21:23:01 +0530

    assigining val & printing type

---
+++ day 10/prob 3.py
@@ -0,0 +1,23 @@
+#assigning an integer value to a variable
+x=10
+print(x)
+print(type(x))
+
+#assigning a float value to a variable & type of variable
+y=3.14
+print(y)
+print(type(y))
+
+#assigning a string value to a variable&type of variable
+z="Hello"
+print(type(z))
+
+#printing type of variable
+a = True
+print(type(a))
+
+b = "hello"
+print(type(b))
+
+c =5+3j
+print(type(c))
