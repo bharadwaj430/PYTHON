391f49fd80dfcd56a05edff0800c0bb7010592d6
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-14 20:06:30 +0530

    Added paraameter example

---
+++ day11/prblm3.py
@@ -0,0 +1,14 @@
+#parameter example
+def add(a, b):
+    add_result = a + b
+    print("Addition :", add_result)
+def sub( a, b):
+    sub_result = a - b
+    print("Subtraction :", sub_result)
+def mul(a,b):
+      mul_result = a * b 
+      print("Multiplication :", mul_result)
+    
+add(5, 3)
+sub(10, 4)
+mul(6, 7)
