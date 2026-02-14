fb4e2f2780cc80226f5cd28911d73aa1a6383bd3
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-14 19:43:23 +0530

    Added local& diff b/w local and glb var

---
+++ day11/prblm2.py
@@ -0,0 +1,15 @@
+#local variable
+def greet():
+  message = "hey! How are you doing?"
+  print(message)
+greet()
+
+#diff between local and global variable
+x = 10  # Global variable
+
+def func():
+    x = 5   # Local variable
+    print("Inside:", x)
+
+func()
+print("Outside:", x)
