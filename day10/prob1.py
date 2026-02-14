3bfc05cbfa4e9630a095daf5f28b7ca0a5a04920
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-14 13:18:50 +0530

    Added variables

---
+++ day10/prob1.py
@@ -0,0 +1,10 @@
+#printing variables and assigning values to them
+x = 10
+print(x)
+a = 5 ; b = 7
+print(a, b)
+full_name = "bharadwaj marri"
+print(full_name)
+print(full_name.upper())
+sgpa = 8.5
+print(sgpa)
---
+++ day10/prob5.py
@@ -0,0 +1,24 @@
+myvar = "bharat"
+my_var = "bharat"
+_my_var = "bharat"
+myVar = "bharat"
+MYVAR = "bharat"
+myvar2 = "bharat"
+print(myvar)
+print(my_var)       
+print(_my_var)
+print(myVar)  
+print(MYVAR)
+print(myvar2)
+
+
+
+#output :
+"""
+bharat
+bharat
+bharat
+bharat    
+bharat
+bharat
+"""
