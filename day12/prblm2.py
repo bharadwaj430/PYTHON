a5c4b3041c23437684ec1eff337dc25b7dcde431
bharadwaj430 <bharadwajmarri@gmail.com>
2026-02-17 19:29:36 +0530

    Added i/p and square of it

---
+++ day12/prblm2.py
@@ -0,0 +1,8 @@
+#accept an  integer aas input and print its square as output
+x = int(input("Enter an integer: "))
+print(x**2)
+
+#accept two  integers as input and print thier sum as output
+y = int(input("Enter first integer: "))
+z = int(input("Enter second integer: "))
+print(y+z)
