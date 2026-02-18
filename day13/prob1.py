e3b86c5746f54d24a48a0425f3ffb866c821e020
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-02-18 19:21:00 +0530

    Added string positioning

---
+++ day13/prob1.py
@@ -0,0 +1,15 @@
+#problems on strings ,datatypes
+#printing length of string
+m = "Consistency.ai"
+print(len(m))
+
+#index notation
+#printing first character
+print(m[0])
+
+#printing last character
+print(m[-1])
+
+#printing 3rd character
+print(m[2])
+
