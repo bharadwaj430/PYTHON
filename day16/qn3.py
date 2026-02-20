e8752f460aa6327549fbc0dbaa82c47d91b5165b
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-02-20 19:43:50 +0530

    calculatingremainder in div

---
+++ day16/qn3.py
@@ -0,0 +1,5 @@
+# Find the remainder when one number is divided by another. 
+dividend = float(input("Enter the dividend: "))
+divisor = float(input("Enter the divisor: "))
+remainder = dividend % divisor
+print(f"The remainder when {dividend} is divided by {divisor} is {remainder}.")
