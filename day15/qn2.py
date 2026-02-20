49090ab608a738dc2fdcfbf02d4045c0957a8579
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-02-20 19:08:08 +0530

    calculating A & P of rectangle

---
+++ day15/qn2.py
@@ -0,0 +1,17 @@
+#basic inputs needed for rectangle
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+
+#calculating area of rectangle
+area = length * width
+
+#calculating perimeter of rectangle
+perimeter = 2 * (length + width)
+
+#printing the area and perimeter of rectangle
+print("The area of the rectangle is", area)
+print("The perimeter of the rectangle is", perimeter)
+
+
+
+
