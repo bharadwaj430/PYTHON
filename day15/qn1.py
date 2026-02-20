c52352ea7b25529e8705a7bba2f7200d91e60bf5
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-02-20 19:01:25 +0530

    basic operations in str

---
+++ day15/qn1.py
@@ -0,0 +1,18 @@
+#take two numbers from user and print thier operations
+#printing sum of two numbers
+x = 94
+y = 6
+sum = x + y
+print("The sum of", x, "and", y, "is", sum)
+
+#printing difference of two numbers
+difference = x - y
+print("The difference of", x, "and", y, "is", difference)
+
+#printing product of two numbers
+product = x * y
+print("The product of", x, "and", y, "is", product)
+
+#printing quotient of two numbers
+quotient = x / y
+print("The quotient of", x, "and", y, "is", quotient)
