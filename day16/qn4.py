a602389c108791ddcde25923d54e1865e2fd7f62
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-02-20 20:05:52 +0530

    Added floor division

---
+++ day16/qn4.py
@@ -0,0 +1,29 @@
+#analyzing and understanding the floor division operator
+x = 17
+y = 3
+result = x // y
+print("The result of floor division is:", result)
+print("The type of result is:", type(result))
+
+#what if floar division with float
+#type 1 one is float and other is int
+x = 17.5
+y = 2
+result = x // y
+print("The result 1  of floor division with float is:", result)
+print("The type of result 1 is:", type(result))
+
+#type 2 one is int and other is float
+x = 17
+y = 2.5
+result = x // y
+print("The result 2 of floor division with float is:", result)
+print("The type of result 2 is:", type(result))
+
+#type 3 both are float
+x = 17.5  
+y = 2.5
+result = x // y 
+print("The result 3 of floor division with float is:", result)
+print("The type of result 3 is:", type(result))
+
