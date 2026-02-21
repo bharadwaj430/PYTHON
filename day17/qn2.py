60058909c67e98a0c7bb3621d5bbce6475450ce5
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-02-22 00:10:48 +0530

    parts of complex no.

---
+++ day17/qn2.py
@@ -0,0 +1,18 @@
+""""
+accessing parts of complex number
+1.Given z = 6 + 9j,
+#  print only the real part and only the imaginary part separately
+""" 
+z = 6 + 9j
+print("Real part:", z.real)
+print("Imaginary part:", z.imag)
+
+# 2. check data type of real and imaginary parts of z
+print(type(z.real))
+print(type(z.imag))
+
+#3.Negative complex no parts
+z = -3 - 4j
+print( z.real)
+print(z.imag)
+
