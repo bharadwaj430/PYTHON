216bf067b033883cc8d4a1ff53b4a00ca99cc24a
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-02-21 20:25:59 +0530

    added complex no

---
+++ day17/qn1.py
@@ -0,0 +1,22 @@
+# 1.Create a complex number 3 + 4j and print it.
+#  Check its type using type()
+z = 3 + 4j  
+print(z)       
+print(type(z))  
+
+
+# 2. Create a complex number using the complex() function 
+# with real part 5 and imaginary part 2. Print it.
+z = complex(5, 2)
+print(z) 
+
+
+# 3. What is the output of complex(7)
+#  — a number with no imaginary part
+z = complex(7)
+print(z)
+
+
+# 4. printing  complex(0, 0)
+z = complex(0, 0)
+print(z)  
