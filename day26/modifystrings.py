c528b60b65788342bee599369bf0bdd07c5435a7
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-04 18:06:29 +0530

    Modified str

---
+++ day26/modifystrings.py
@@ -0,0 +1,17 @@
+a = "Hello, World!"
+
+#print uppercase letters
+print(a.upper()) 
+
+#print lowercase letters
+print(a.lower())
+
+
+ # removes whitespace from the beginning and the end
+print(a.strip())
+
+#replace a string with another string
+print(a.replace("H", "J"))
+
+#split the string into a list
+print(a.split(","))
