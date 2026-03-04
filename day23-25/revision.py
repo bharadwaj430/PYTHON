0de5fe78f793cd18265a572c419264fc74f738e4
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-04 17:54:34 +0530

    revision

---
+++ day23-25/revision.py
@@ -0,0 +1,12 @@
+
+#revision of all the concepts for interview purpose and
+# clarifications of mistake done in the past
+"""
+python syntax
+py output
+py comments
+py variables
+py data types
+py numbers
+py casting
+""" 
