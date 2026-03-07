6e07ea2d114eb9af47700dae4d0954ccca2f4ca5
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-07 16:18:27 +0530

    Added  string quotes diff

---
+++ day28/intq3.py
@@ -0,0 +1,17 @@
+"""
+What is the difference between single, double, and triple quotes in Python strings?
+
+Single (') and double (") quotes can be used interchangeably for creating strings.
+ The choice between them often depends on the need to include quotes of one type inside the string without escaping them.
+
+ Triple quotes allow for multiline strings and are commonly used for docstrings (function or module documentation).
+
+"""
+single_quote_string = 'hi everyone.'
+print(single_quote_string)
+
+double_quote_string = "done with today's work."
+print(double_quote_string)
+
+triple_quote_string = '''Calculated risk leads to success'''
+print(triple_quote_string)
