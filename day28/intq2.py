e6a6f8d19b1f38fd2794ba52d057b2e2de47ee19
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-07 15:58:35 +0530

    Added quotes in string

---
+++ day28/intq2.py
@@ -0,0 +1,27 @@
+"""
+1. What are strings in Python and how are they created?
+in Python, strings are sequences of characters enclosed within quotes. 
+They are used to store text data and are a built-in data type. 
+Strings can be created using single quotes ('), double quotes ("), or triple quotes 
+Triple quotes are often used for multiline strings or docstrings.
+"""
+# Single-quoted string
+single_quote = 'Hello, World!'
+print(single_quote)
+
+# Double-quoted string
+double_quote = "Hello, Python!"
+print(double_quote)
+
+# Triple-quoted string (multiline)
+triple_quote = '''This is
+a multiline string.'''
+print(triple_quote)
+
+#strings in Python are objects of the str class and can be created using the str() constructor as well
+# Using str() constructor
+string_from_constructor = str("Hello, This is programming in vs code!")
+print(string_from_constructor)
+
+
+
