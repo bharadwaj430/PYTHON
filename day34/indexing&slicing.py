93c711ee92fe528e0989272ce51f1e03bf7fcb54
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-14 01:14:10 +0530

    Indexing,slicing in str

---
+++ day34/indexing&slicing.py
@@ -0,0 +1,14 @@
+#access specific characters through positivei indexing
+s = "beginners"
+#positive indexing
+print(s[0])   
+print(s[4])
+#negative indexing
+print(s[-6])
+print(s[-3])
+#string slicing
+s = "GeeksforGeeks"
+print(s[1:4])    
+print(s[:3])     
+print(s[3:])    
+print(s[::-1])
