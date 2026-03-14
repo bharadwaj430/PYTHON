b1a585fc1eed2044ded57ccc74d29a6db31bbd67
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-14 19:54:50 +0530

    printing str with details

---
+++ day35/task2.py
@@ -0,0 +1,24 @@
+# print a string "Hi I'm Bharadwaj gupta studying Btech IT with 65%"
+#fun(name="Bharadwaj",course="Btech", branch="IT", perc=65%)
+
+#find the prime numbers for the given range---100
+#functions--define,parameter, return,print,
+# *args-if we don't know how many no.of parameters will be pass by the user
+def fun(fname,lname):
+    print("Hi Im"+" "+fname+" "+lname)
+fun(fname='bharadwaj',lname='gupta')
+
+def fun1(fn,ln):
+    print("Hlo"+ fn + ln)
+fun1('how',  'are you')
+
+def fun(*names):#*args
+    return "Hi my name is"+ "satish " 
+
+print(fun("a","b","c","d"))
+
+
+def fun1(**names):
+    return "Hi my name is"+" "+names["lname"]
+print(fun1(fname="bharat",lname="kumar"))
+    
