38f76c96f7f55331ad156a49a4c5bc795177a0f4
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-15 19:26:24 +0530

    print prime no

---
+++ day35/task2(1).py
@@ -0,0 +1,11 @@
+#find the prime numbers for the given range---100
+#functions--define,parameter, return,print,
+# *args-if we don't know how many no.of parameters will be pass by the user
+begin_num = int(input("Enter begin_num: "))
+end_num = int(input("Enter end_num: "))
+
+primes = [n for n in range(begin_num, end_num+1) if n > 1 and all(n % i != 0 for i in range(2, n))]
+
+print(primes)
+
+
