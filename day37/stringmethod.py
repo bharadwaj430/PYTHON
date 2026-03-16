f6e57d06c34f1743778d6acd55d4bd177d13fea6
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-16 07:30:21 +0530

    Added str methods

---
+++ day37/stringmethod.py
@@ -0,0 +1,32 @@
+#string methods
+# 1. len()-string length
+m = "productivity"
+print(len(m))
+
+# 2. upper()- Converts all uppercase characters in a string into lowercase,
+#    lower()-Converts all lowercase characters in a string into uppercase
+l = "experience exchange"
+print(l.upper())
+print(l.lower())
+
+# 3. strip() , replace()
+s = "   information technology  "
+print(s.strip())    
+
+b = "Python is fun"
+print(b.replace("fun", "awesome"))
+
+# 4. title()-Convert string to title case
+j ="Bharadwaj"
+print(j.title())
+
+
+# 5. swapcase() - Swap the cases of all characters in a string
+e ="life"
+print(e.swapcase())
+# 6. capitalize()-Convert the first character of a string to uppercase
+f = 'bharadwaj'
+print(e.capitalize())
+
+
+
