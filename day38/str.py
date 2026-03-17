761a89f81bc1ab88da47c663c46c220c3504f61a
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-17 23:57:17 +0530

    Added last position of substr

---
+++ day38/str.py
@@ -0,0 +1,6 @@
+#last position of given substring
+str1 = "he is an ai engineer who knows Python. he works at google."
+print("Original String is:", str1)
+
+index = str1.rfind("he")
+print("Last occurrence of he  starts at index:", index)
