39a658b4899fa11f4956a11e181a374a419e8477
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-20 23:11:21 +0530

    Added documentation

---
+++ day41-42/detail.py
@@ -0,0 +1,5 @@
+#i have covered all the concepts ,revised , and prepared for interview to be held on every month , 29th march-this month
+"""
+practice done for the concepts which i have found weak
+learning the concept and understanding in better way
+"""
