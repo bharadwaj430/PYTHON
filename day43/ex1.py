72453f3ce9c2303ae2ccfa8ef0332dfc19f9811e
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-20 23:34:47 +0530

    Added str methods

---
+++ day43/ex1.py
@@ -0,0 +1,12 @@
+s = "python seems to be interesting"
+m = "I use it in AI/ML"
+print(len(s)) #len()-total characters of string
+print(s.upper()) #upper()-converts all char to uppercase
+print(s.lower())#lower()-converts all to lower case
+print(s.strip())#remove spaces and trailing 
+print(s.replace("fun", "awesome"))#replaces the unwanted ones.
+print(s + " " + m) #concatenate str +
+print(m * 3) #repeat string
+
+
+
