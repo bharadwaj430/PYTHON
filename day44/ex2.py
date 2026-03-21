1e3374921b08b3cba03124531540cde826648fdd
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-03-22 00:00:50 +0530

    FORMATTING USING FORMAT()

---
+++ day44/ex2.py
@@ -0,0 +1,6 @@
+#formatting using format()
+s = "My name is {} and I am {} years old.".format("sai", 12)
+print(s)
+
+m = "I am studying in {} high school in {}.".format("government","khammam")
+print(m)
