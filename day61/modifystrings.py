4e29ed8296d6f8c84a984f747552c0a5ddd4faa2
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-04-07 21:02:19 +0530

    added modifystrings

---
+++ day61/modifystrings.py
@@ -0,0 +1,25 @@
+#printing uppercase in string modification
+a = "IS bharadwaj At Consistency ai?"
+print(a.upper()) 
+#o/p : IS BHARADWAJ AT CONSISTENCY AI?
+
+
+#printing lowercase in string modification
+b ="IS bharadwaj At consistency ai?"
+print(b.lower())
+# O/P : is bharadwaj at consistency ai?
+
+
+#removing whitespace
+c = "bharadwaj at consistency ai?"
+print(c.strip())
+# O/P : bharadwaj at consistency ai?
+
+#replacing a string with another string
+d = "IS bharadwaj at consistency ai?"
+print(d.replace("b","B" ))
+#o/p : IS bharadwaj at consistency ai?
+
+m = "IS bharadwaj at consistency ai?"
+print(m.split(","))
+#o/p :["IS bharadwaj at consistency ai?"]
