f4ee2baa4fb8327defd2ff3c8291e516cb816b4c
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-04-13 20:26:17 +0530

    added decorator operations

--- day65-66/d1.py
+++ day65-66/d1.py
@@ -7,10 +7,21 @@ def decorator_name(func):
         return result
     return wrapper
 
-#decorator name
+@decorator_name
 def add(a,b):
     return a+b
 print("The result is : ", add(23,9))
+@decorator_name
+def sub(a,b):
+    return a-b
+print("The result of subtraction:" ,sub(23,9))
+@decorator_name
+def mul(a,b) :
+    return a*b
+print("the result of multiplication is :", mul(23,9))
+@decorator_name
+def div(a,b):
+    return a/b
 
 
 
