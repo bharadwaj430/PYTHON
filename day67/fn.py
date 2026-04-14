2236938f3126c479f6efdbdc48c5412d9a383780
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-04-14 23:41:10 +0530

    added fns

---
+++ day67/fn.py
@@ -0,0 +1,18 @@
+
+"""
+functions are block of statements for specific task.
+idea is to repeatedly tasks put together and making a fn.
+fn calls to reduce reuse of code writing
+py fns are
+#1 code reusable
+#2 modular-breaking into modules
+redable and maintainable
+"""
+def oddoreve(x):
+  if(x%2==0):
+    return "eve"
+  else:
+    return "odd"
+print(oddoreve(26))
+print(oddoreve(7))
+
