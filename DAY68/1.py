82dd45cd40374bf086a99f2d82119c4fb01941f3
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-04-17 23:55:48 +0530

    added fn

--- DAY68/1.py
+++ DAY68/1.py
@@ -1,10 +1,6 @@
-#forward recursion concept
-def printNumbers(number , n):
-    if number > n : # Base Case
-        return
-    print(number , end=' ')
-    printNumbers(number , n) 
-n = 7
-number  = 10
-for n in range (0,n):
- printNumbers( number ,n)
+#default arguments
+def myFun(x, y=50):
+    print("x: ", x)
+    print("y: ", y)
+
+myFun(10)
