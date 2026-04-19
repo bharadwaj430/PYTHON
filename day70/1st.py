4f108c6da8f89aa2b552e067c651220209d7917e
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-04-19 18:52:35 +0530

    added string datatype

---
+++ day70/1st.py
@@ -0,0 +1,17 @@
+#data types in python
+"""
+datatypes in python is classified as 
+1.numeric has subtypes i)integer ii)float iii)complex number  
+2.dictionary
+3.boolean
+4.set
+5.sequence type further divided string , tuple , list
+"""
+d  = 'Welcome to the workspace'
+print(d)
+
+print(type(d))
+
+print(d[1])
+print(d[2])
+print(d[-1])
