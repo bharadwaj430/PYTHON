9db268fbf3b0bdceea20efe010089a030fffd095
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-04-21 23:07:54 +0530

    added access list items

---
+++ day71/2.py
@@ -0,0 +1,11 @@
+#access list items
+"""
+#allocating the address of list elements to 
+give a perfect alignment to code
+#in python negative sequence indexes 
+"""
+list = [1,2,3,4,5,6]
+print(list[-1]) #negative indexing
+print(list[3])#+ve indexing
+print(list[0]) #0 as index
+
