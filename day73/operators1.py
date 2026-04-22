891a09517ee5d02ed211ebbb81ddcc342074dd97
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-04-22 22:45:30 +0530

    added operators

---
+++ day73/operators1.py
@@ -0,0 +1,20 @@
+#operators in python
+"""
+arithmentic operators +,-,*,/,%
+relational operators <,<=,><+,==,!=
+logical operators AND,OR,NOT
+bitwise operators &,|,^,~,<<,>>
+assignment operators =,+=,-=,*=,%=
+"""
+
+#comparison/relational  operators in python-return T/F
+a = 13
+b = 33
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
