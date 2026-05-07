806e0db9b8648357339692fc00e0352c2f207f03
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-07 19:04:43 +0530

    added keywords

---
+++ day74/1.py
@@ -0,0 +1,48 @@
+#keywords in python
+"""
+keywords are special reserved words that are part of langauage itself
+they define rules and structures of python 
+"""
+#code to get list of all python keywords
+import keyword
+print("The list of keywords are : ")
+print(keyword.kwlist)
+
+"""
+output :
+list of keywords are
+false 
+none
+as
+and
+assert
+async
+await
+break
+continue
+def
+del
+elif
+else
+except
+finally
+for
+from
+global
+if
+import
+in
+is
+lambda
+non local
+not
+or
+pass
+raise
+return
+try
+while
+with
+yield
+
+"""
