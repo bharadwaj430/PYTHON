73eac7399de548cf8d6b9a3ea709722975340fba
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-07 19:59:08 +0530

    added list  datatype

---
+++ day74/3.py
@@ -0,0 +1,14 @@
+"""
+list data type: similar to arrays in other lang
+list creation : sequence inside square brackets
+"""
+# Empty list
+a = []
+
+# list with int values
+a = [1, 2, 3]
+print(a)
+
+# list with mixed values int and String
+b = ["exams", "For", "end semester", 4, 5]
+print(b)
