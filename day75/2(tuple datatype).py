e1dd084ab0a3ad2c180388a3b9b278f0e3cec39d
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-08 18:53:51 +0530

    tuple datatype

---
+++ day75/2(tuple datatype).py
@@ -0,0 +1,11 @@
+"""
+tuple datatype : ordered collection of python objects.
+"list vs tuple only difference"
+tuple - immutable(cannot be changed after created)
+list - mutable (can be changed after created)
+"""
+
+#empty tuple
+tuple = ()
+tuple2 = ("bharadwaj" , 'is' , 'a', ' student')
+print("tuple using a string:", tuple2)
