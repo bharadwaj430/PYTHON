55890c6531453c45511c8308720031a0928228c6
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-08 18:38:33 +0530

    access list items

---
+++ day75/1(access list items).py
@@ -0,0 +1,14 @@
+# we refer index no to access list items)
+"""
+-ve indexing from end of array, starting sequence beginning from end
+list[index no] - syntax for index number in list
+
+"""
+a = ["student", "is", "consistent"]
+print("Accessing element from the list using indexing")
+print(a[0])
+print(a[2])
+
+print("Accessing element using negative indexing")
+print(a[-1])
+print(a[-3])
