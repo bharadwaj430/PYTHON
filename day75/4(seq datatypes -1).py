b3cb1f02381cc58ed4736aa48cf6ef2e8849434e
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-08 19:30:37 +0530

    len of seq datatype

---
+++ day75/4(seq datatypes -1).py
@@ -0,0 +1,22 @@
+#finding length of sequence datatype
+
+#finding length are two kinds
+"""
+1. finding no of items
+2.finding memory size
+"""
+#1. finding no of items 
+
+# # (i) length of string  - counting characters
+s = "python"
+print("length of given string is :", len(s))
+
+#(ii)length of list - counting elements
+list = [4,5,6,1,0,0,7]
+print("length of given list :" , len(list))
+
+#(iii) length of tuple
+tuple1 = ("tomato" ,"ashgourd" , 'brinjal')
+print("length of given tuple is : " , len(tuple1))
+
+
