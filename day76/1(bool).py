6a65f64d5a361c3a654c7f8811b51ebc0dea1078
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-09 20:02:50 +0530

    bool datatype

---
+++ day76/1(bool).py
@@ -0,0 +1,18 @@
+"""
+boolean datatype :
+1.  boolean one of two built in values, true or false
+2. boolean objects = true are truthy
+                   = false are falsy
+non boolean objects can be evaluated in boolean context (t or f)
+it is indicated by class bool
+
+"""
+
+#truth and falsy are values that evalauate true or false in boolean context
+#truthy val = true , falsy val = false
+
+if 1:
+    print("1 is truthy")
+
+if not 0:
+    print("0 is falsy")
