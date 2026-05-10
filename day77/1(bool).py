8ec0c05ed9b9772d26a839d36850b5778cf96fb6
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-10 21:09:02 +0530

    set datatype

---
+++ day77/1(bool).py
@@ -0,0 +1,15 @@
+#determining the class of boolean datatype
+print(type(True))
+
+"""
+SET DATATYPE : set is unordered collection of datatypes
+
+"""
+# initializing empty set
+s1 = set()
+
+s1 = set("groupofcolleges")
+print("Set with the use of String: ", s1)
+
+s2 = set(["bhaskar", "engineering", "college"])
+print("Set with the use of List: ", s2)
