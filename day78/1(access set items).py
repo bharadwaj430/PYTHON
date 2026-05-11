2a1e6e2fca12d3eb7c47ac0e46b7fcc1bb8913f9
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-11 23:01:38 +0530

    access set items

---
+++ day78/1(access set items).py
@@ -0,0 +1,8 @@
+set1 = set(["peer", "For", "peer"]) #Duplicates are removed automatically
+print(set1) 
+# loop through set
+for i in set1:
+   print(i, end=" ") #prints elements one by one
+  
+# check if item exist in set   
+print("peer" in set1)
