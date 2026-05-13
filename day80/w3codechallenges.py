0da8a7dc32310ad5d82c1937a9c422040aa93523
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-13 18:52:57 +0530

    code prblms

---
+++ day80/w3codechallenges.py
@@ -0,0 +1,34 @@
+# code challenges  in datatypes
+x = 5
+y = 3.14 #pi value = non terminating non recurring decimal
+z = "bharadwaj"
+#print the  type of each variable
+print(type(x))
+print(type(y))
+print(type(z))
+"""o/p:
+<class 'int'>
+<class 'float'>
+<class 'str'>
+"""
+#type conversion
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
