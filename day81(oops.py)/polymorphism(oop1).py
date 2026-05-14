a53afbf99d76622a75a69fb2de2024d19d8b7bca
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-14 23:50:17 +0530

    POLYMORPHISM

---
+++ day81(oops.py)/polymorphism(oop1).py
@@ -0,0 +1,49 @@
+#under the concept of python classes
+#PYTHON OOP
+#OBJECT ORIENTED PROGRAMMING
+"""
+py is obj oriented lang
+helps to structure code by classes and obj
+better organization and reusability
+
+ADV OF OOP
+clear struct of programs
+code easy
+reuse
+debug
+code DRY FACTOR  (don't repeat yourself)
+The DRY (Don't Repeat Yourself) principle in Python
+ is a S/w dev  philosophy aimed at
+   reducing reps  of info or logic. 
+   It gives maintainable , readable, and efficient
+  single
+  unambigious - clear way to intrepret data/code
+"""
+#polymorphism
+#function polymorphism
+#An example of a Python function that can be used on different objects is the len() function.
+z = "bharadwaj" #string
+print(len(z))
+
+
+mytuple = ("bharadwaj", "pursuing", "engineering") #tuple
+print(len(mytuple))
+
+thisdict = {              #dict
+  "bharadwaj": "student",
+  "college": "going",
+  "pass out": "24-28"
+}
+
+print(len(thisdict))
+
+"""
+output:
+9   #for string
+3    #for tuple
+3   #for dict
+"""
+
+
+
+
