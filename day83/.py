54de1f8f91d9aebd948621e601dec5901356c6df
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-16 22:07:18 +0530

    added pass statement

---
+++ day83/.py
@@ -0,0 +1,10 @@
+#pass statement
+"""
+class definitions can't be empty
+if we have pass definition with no content
+"put in pass statement tp avoid getting error"
+"""
+#ex:
+class Person:
+  pass
+
