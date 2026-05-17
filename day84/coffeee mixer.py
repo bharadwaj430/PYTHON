ba926f22ca7c0e0020c0ae15f1186dd43d4250de
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-17 23:31:54 +0530

    added coffee mixer prblm

---
+++ day84/coffeee mixer.py
@@ -0,0 +1,27 @@
+# information given in coffee mixer
+"""
+current = 5 A (amperes)(I) 
+resistance = 3  , measured in opp flow of current
+V=IR (voltage = current(watts) + resistance(amperes))
+"""
+voltage = 5  #V
+current = 3  #I
+#calculate resistance
+R = voltage / current
+print(R)
+
+
+#output:
+"""
+manual calculation: 1.65
+vs code run calcualtion = 1.6666666666666667
+so the final answer is (approx.) ~ 1.6
+"""
+
+
+
+
+
+
+
+
