b5615a4263aa13cc9412af0ce151d54316876e2e
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-26 15:58:19 +0530

    added built in fns

---
+++ 26may2026/builtinscope.py
@@ -0,0 +1,4 @@
+#built in scope refers to all of python built in functions , modules and keywords and are available anywhere
+print(str(45)) # '45'
+print(type(3.14)) # <class 'float'>
+print(isinstance(3, str)) # False
