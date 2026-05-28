4a09b48b1e2c7e66cffa437e28eb7d148f9c4baf
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-28 15:47:26 +0530

    list datatypes

---
+++ 28may2026/listdatatypes.py
@@ -0,0 +1,27 @@
+#a list can be of any datatype
+list1 = [5,8,10,12,13,15]
+list2 = ["bharath" , "sai" , "shiva" , "yadagiri"]
+list_3 = [True,False, False]
+
+#a list can be a combination of integers , boolean values and strings
+taken_list = ["bharath",7, True]
+print(taken_list) 
+
+#type()
+i_have_a_list =["goat", "cow","sheep"]
+print(type(i_have_a_list))   #<class 'list'>
+
+#the list() constructor
+#used when creating a new list
+random_list = list(("consistency ai ", "student tribe", 'masters union'))
+
+#python collections
+#four collection datatypes
+#list = ordered and changeable 1. allow duplicates
+#tuple = ordered and unchangeable allow duplicates
+#set = unordered , unchangeable
+#dictionary = ordered and changeable , no duplicates
+
+
+
+
