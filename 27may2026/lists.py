69a8d6ad19762eb2ae1e180fed0b28221d0d6b00
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-28 15:21:50 +0530

    added lists

---
+++ 27may2026/lists.py
@@ -0,0 +1,38 @@
+"""
+#used to store multiple items in a single variable
+#list is one of 4 built in datatypes
+#it is used to store collections of data
+#1.tuple , 2.set 3.dictionary 4.list
+
+"""
+students_list = ['bharath' , 'mohan' , 'chaitanya']
+print(students_list)
+
+#list items
+#list items are ordered , changeable , allow duplicate values
+# indexed like 0,1,2,3,4
+
+""""
+ordered list
+items are defined in order
+when we add new items , they are placed at end of list
+"""
+
+#lists are changeable 
+#1.change 2. add 3.remove items in list
+#it allows duplicates , if are indexed , lists can have items with same value
+
+
+i_have_list = ['dog','cat','lion','cat','goat']
+print(i_have_list)
+
+#length of the list
+#to find the length of list we use len fn
+here_list = ['laptop','pendrive','harddisk']
+print(len(here_list))  #3
+
+
+
+ 
+
+ 
