f21ca33e2065868d074fcb9d91ea9c8d7b17385c
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-29 11:45:51 +0530

    variables rev

---
+++ 29may2026/1.py
@@ -0,0 +1,54 @@
+#python 
+#it is simple and easy like  english
+#it is free and open source (anyone can use)
+#it is a high level langauage 
+#developed by guido van rossum
+#it is a portable language as cross platform os independency
+#we can switch multiple domains with help of python
+#it is a versatile language
+
+#takes input and gives output  #google search example
+
+#python character set  - extra characters other than these can't be used
+
+"""
+A to Z letters (capital)
+a to z (small)
+digits 0 to 9 
+special symbols , + , - , *,/ etc..
+whitespaces - blank space , tab , carriage return,new line , form feed
+other characters - All ascii(american standard code for information exchange) and unicode characters as part of data literals
+"""
+
+print("this is bharadwaj")
+print("my age is 20")
+print("i am bharadwaj", "my age is 20")
+print(20,35)
+print(35 +23)   #sum - operations in python
+ 
+
+#variables
+#a variable is a name given to a memory location in a program
+#string - word ,sentence ,multiple words written in prog lang in double quotes/single quotes
+name = 'bharadwaj'  # name is variable and bharadwaj is value
+age = 20 
+marks = 95
+print("my name is :" , name)
+print("my age is :", age) #20
+print("age") #age
+print(marks)
+
+
+#computer has slots of memory we visualize the empty spaces
+#storing the name in memory allocations
+
+
+
+age  = 23 # right side val stored in left side value
+#(=) is  assignment operator
+age2 = age
+print(age2)
+
+
+  
+
