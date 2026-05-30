6844ee2e9552ca0cba3461b0435be502f8e5a882
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-30 10:59:43 +0530

    datatypes

---
+++ 30may2026/datatypes.py
@@ -0,0 +1,24 @@
+#1 integers - =ve,-ve,0
+#2 string - "bharadwaj","hello"
+#3 float - 3.99 , 2.5 , 9.0
+#4 boolean -true , false
+#5 none - no value storage ex : a = none
+
+#strings
+
+name1 = "bharadwaj"
+name2 ='bharadwaj'
+name3 = ''' bharadwaj'''
+print(name1)
+print(name2)
+print(name3)
+
+age = 23
+old = False   #bool type should be capital
+a = None
+print(type(age))
+print(type(old))
+print(type(a))
+
+
+
