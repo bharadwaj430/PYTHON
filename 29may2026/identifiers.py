e37b2385faeee78c301f54b9028c53bae9ef79f9
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-30 10:47:14 +0530

    identifiers

---
+++ 29may2026/identifiers.py
@@ -0,0 +1,31 @@
+#rules of identifiers
+#1identifiers are combination of uppercase, lowercase , digits or underscore(_)
+# myVariable , variable_1, variable_for_print -are valid python identifiers
+#3 we can't use special symbols like !,#,@,%,$ etc in our identifier
+#4 identifier can be of any length
+
+#variable1 - valid
+#1variable - not valid
+
+thisismypythonvariable = 5  #acceptable code
+#variable names should be simple to unnderstand, short ,meaningful
+
+a = 5
+b = "college"
+c = 19.5
+d = -25
+print(thisismypythonvariable) #5
+print(type(a)) #<class 'int'>
+print(type(b)) #<class 'str'>
+print(type(c))#<class 'float'>
+print(type(d)) 
+
+
+
+
+
+
+
+
+
+ 
