38d1ba950261496aa708e221a949f882264cdccb
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-05-30 13:48:52 +0530

    added operators

---
+++ 30may2026/1.py
@@ -0,0 +1,69 @@
+#arithmetic operators - (+,-,*,/,%,**)
+a = 5
+b = 2
+sum = a + b
+print(sum) #7
+
+
+print(a+b) #7
+print(a-b) #3
+print(a*b) #10
+print(a/b) #2.5
+
+#modulo operator
+print(a%b) #1  is remainder
+print(a**b) #a^b is 25
+
+#relational/comparison operators(==,!=,<,>,<=,>=)
+a = 15
+b = 20
+print(a==b) #False
+print(a!=b) #True
+print(a<b) #True
+print(a>b) #False
+print(a>=b) #False
+print(a<=b) #True
+
+#assignment operators - (=,+=,-=,*=,/=,%=,**=)
+num = 10
+num += 10 #num = num + 10 = 10 + 10 = 20
+print("number is", num)  #20
+
+value = 20
+value -= 20  #value = value - 20 = 20-20 = 0
+print(value) #0
+
+integer = 30
+integer *= 30 # integer = integer * 30 = 30*30 = 900
+print(integer)
+
+
+number = 50
+number/= 50 # number = number /50 = 50/50 =1
+print(number) #1.0
+
+the_number = 70
+the_number %= 70 # the_number = the_number % 70 =70 %70 = 0
+print(the_number)   #0
+
+
+is_number = 6
+is_number**= 6  #is_number = is_number **6 = 
+print(is_number) #46656
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
