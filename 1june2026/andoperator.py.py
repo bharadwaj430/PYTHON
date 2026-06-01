8c87e0affbb6a77137d15515e19f1a3fed382118
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-01 22:44:35 +0530

    and operator

---
+++ 1june2026/andoperator.py.py
@@ -0,0 +1,18 @@
+#logical operators
+#it is a symbol that performs a certain operations between operand
+#1.NOT , AND ,OR
+
+
+a = 50
+b = 30
+
+
+print(not False) #True 
+print(not True) #False
+print(not (a > b))   #False
+
+
+value1 = True
+value2 = True
+print("logical operators:", value1 and value2)  #True  
+
