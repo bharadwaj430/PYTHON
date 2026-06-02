2b1a991008a71c38452b1dcbaa541159a1892ec6
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-02 17:46:39 +0530

    logical operators

--- 1june2026/andoperator.py.py
+++ 1june2026/andoperator.py.py
@@ -13,6 +13,13 @@ print(not (a > b))   #False
 
 
 value1 = True
-value2 = True
-print("logical operators:", value1 and value2)  #True  
+value2 = False
+print("AND  operators:", value1 and value2)   #False
+print("OR  operators condition 1:", value1 or value2)  #True
+print("OR operator condition 2:", (a == b) or value2)  #False 
+print("OR operator condition 3:", (a == b) or (a > b )) #True
+print("NOT operator:" ,  not value2 ) #True
+
+
+  
 
