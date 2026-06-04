d10e75b194d05e63e0e040807a90de4fba51ee0d
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-04 17:21:41 +0530

    added indexing

---
+++ 4june2026/indexing.py
@@ -0,0 +1,16 @@
+#indexing
+#b h a r a d w a j    m  a   r   r   i
+#0 1 2 3 4 5 6 7 8 9  10 11  12  13  14
+
+str = "bharadwaj marri"
+print(str[0],str[1],str[2],str[3],str[4],str[5],str[6],str[7],str[8]) #b h a r a d w a j
+#can also be written as
+print(str[0]) #b
+print(str[1]) #h
+print(str[2]) #a
+print(str[3]) #r
+print(str[4]) #a
+print(str[9]) #empty space
+
+#indexing is a position to access characters 
+#special characters , symbols and all will be considered as indexing
