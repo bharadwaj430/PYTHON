35b3fc5d2ff5110fb46703f820efb2ce5d2c8dd5
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-04 11:49:45 +0530

    added string prblms

---
+++ 3june2026/string.py
@@ -0,0 +1,35 @@
+
+"""
+strings
+string is a datatype  that stores a sequence of characters
+Basic operations
+
+concatenation "hello" + "world" -> "helloworld"
+"""
+
+#length of str = len(str)
+str1 = "this is a string"
+str2 = 'i am using string 2'
+str3 = """ bharadwaj marri"""
+
+
+
+#to avoid confusion for compiler in understanding python we use three kinds of quotes
+
+
+#escape sequence characters helps in formatting
+# \n - printing the sentence on a new line
+
+string4 = "we are typing in vs code .\n  we are implementing the python code "
+print(string4)
+"""
+
+output :
+we are typing in vs code .
+ we are implementing the python code
+ """ 
+
+
+
+
+ 
