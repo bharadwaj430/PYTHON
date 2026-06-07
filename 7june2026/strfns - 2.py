b576c47b542a3d99ea15ae195a9fd49c30e0c1c2
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-07 10:51:23 +0530

    added strfns - 2

---
+++ 7june2026/strfns - 2.py
@@ -0,0 +1,21 @@
+#str = " i am bharadwaj from hyderabad , studying python from consistency.ai"
+#print(str.count("from"))  #2 -counts the occurence of substring in a string
+
+
+
+#practice problems
+
+#1 write a program to input user's first name and print its length
+
+
+#name = input("enter your name ") #bharadwaj (name given by user)
+#print("length of your name is", len(name)) #prints length is 9
+
+
+#write a program to find the occcurence of "$" in a string
+str = "the value of dollar is $ i.e 94.95 as $ value is greater than indian rupee "
+print(str.count("$"))
+
+
+
+
