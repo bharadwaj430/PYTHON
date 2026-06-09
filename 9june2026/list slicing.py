afd5544a0cf35c5dcf0f83ea8f7070a190b4199a
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-09 15:14:37 +0530

    added lis slicing

---
+++ 9june2026/list slicing.py
@@ -0,0 +1,11 @@
+#list slicing
+"""
+it is similar to string slicing
+it has sublist like a substring
+"""
+marks = [87,64,33,95,76]
+print(marks[1:4])
+
+print(marks[:4]) #starting index default 0
+print(marks[1:])  #ending index  is taken as default
+print(marks[-3:-1]) # 76,63
