0628cd6c1c0007ce2a8a7336ee08c6c547b77a55
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-11 21:48:43 +0530

    added tuple basics

---
+++ 11june2026/tuples.py
@@ -0,0 +1,37 @@
+#TUPLES
+
+#a built in datatype that lets us create immutable sequence of values
+#lists -------mutable
+#tuples,strings------------- - immutable
+#we use parenthesis to indicate tuple
+
+# tup = (1,2,4,5)
+# print(type(tup[0])) #<class 'int'>
+# print(tup[1]) #2
+# print(tup[0]) #1
+# print(tup[2]) #4
+
+tuple1 = (1) #tuple considered as  integer
+tuple2 = ("visual studio code") #tuple considered as  string
+tuple3 = (5.5) #tuple considered as  float value
+
+print(tuple1)
+print(type(tuple))
+
+print(tuple2)
+print(type(tuple2))
+
+print(tuple3)
+print(tuple)
+
+
+#to want to consider/ percieve  as a tuple (we should add a comma , )
+#Adding a comma at the last is optional. 
+
+
+
+
+
+
+
+
