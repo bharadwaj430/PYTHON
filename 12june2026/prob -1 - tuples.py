22582590ca6e4d90a440b1bf69e7bacde7959069
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-12 20:01:54 +0530

    added prob 1 on tup mthds

---
+++ 12june2026/prob -1 - tuples.py
@@ -0,0 +1,12 @@
+
+"""
+write a program to users to enter names of their three favourite movies and
+store them in a list 
+"""
+
+movie1 = str(input("enter first  movie "))
+movie2 = str(input("enter second movie "))
+movie3 = str(input("enter third movie "))
+
+movielist = [movie1,movie2,movie3]
+print(movielist)
