0561a667b67e49ef3756d12b5854327b17d76941
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-13 14:06:50 +0530

    added tup prblms

---
+++ 13june2026/prob-4.py
@@ -0,0 +1,13 @@
+#1.WAP  TO COUNT the no of students with the "A" grade in tuple given
+
+
+
+grade = ["C","D","A","A","B","B","A"]
+print(grade.count("A")) #3
+
+#2.#STORE the above values in a list and sort them from "A" TO "D"
+grade.sort()
+print(grade) #['A', 'A', 'A', 'B', 'B', 'C', 'D']
+
+
+
