56a58396ee127999a3945a82222e043313f09c59
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-13 13:05:26 +0530

    added tup prblm

---
+++ 13june2026/prob -2.py
@@ -0,0 +1,28 @@
+# movies = []
+# mov1 = input("enter first movie")
+# mov2 = input("enter second movie")
+# mov3 = input("enter third  movie")
+
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+# """
+# enter first movie shutter island
+# enter second moviefight club
+# enter third  movie obsession
+# [' shutter island', 'fight club', ' obsession']
+
+
+#or 
+movies1 = []
+movies1.append(input("enter 1st movie:"))
+movies1.append(input("enter 2nd movie:"))
+movies1.append(input("enter 3rd movie:"))
+
+
+print(movies1)
+
+
