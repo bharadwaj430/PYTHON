fccdcfb07075ea8d93c1e96580dc38925d3f8414
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-19 19:47:54 +0530

    added set

---
+++ 19june2026/set.py
@@ -0,0 +1,25 @@
+myset = {"apple","banana","cherry"}
+#set
+"""
+set are used to store mutliple items in a single variable
+
+it is a built in datatype among 4 to store collections of data
+1.list
+2.tuple
+3.dictionary
+4.set
+
+a set is collection which is unordered,unchangeable,unindexed
+
+set items are unchangeable but we can remove items and add new items
+
+
+sets are written with curly braces
+"""
+
+#creat a set
+set1 = {"mohan","bharath","chaitanya"}
+print(set1) #set 1 is printed
+
+
+
