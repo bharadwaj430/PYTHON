302034d90d22744bbaa2e29d640161e5c6c953d8
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-20 21:55:11 +0530

    access set items

---
+++ 20june2026/access set items.py
@@ -0,0 +1,13 @@
+#loop through set items using for loop
+
+have_set = {"red","yellow","green"}
+
+for y in have_set:
+  print(y)
+
+#checking if an element is present or not
+print("red" in have_set) #True
+
+#checking if element is not present in the set
+print("blue" in have_set)
+
