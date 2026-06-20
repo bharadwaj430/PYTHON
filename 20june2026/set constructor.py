76fa1d7207bf5cfabd140f45ce2e51c401d2166d
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-20 16:17:21 +0530

    set constructor

---
+++ 20june2026/set constructor.py
@@ -0,0 +1,12 @@
+#From Python's perspective, sets are defined as objects with the data type 'set':
+
+myset = {"phone","ATM MACHINE","laptop"}
+print(type(myset)) #<class 'set'>
+
+
+#SET CONSTRUCTOR
+#we can make set() constructor to make a set---use double round brackets
+
+is_set = set(("book","pen","car"))
+print(is_set)  #{'car', 'pen', 'book'}
+
