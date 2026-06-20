3a41939c597e927169a798ab092a87286222a4ae
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-20 15:43:56 +0530

    added set items with a datatype

---
+++ 20june2026/set items-datatypes.py
@@ -0,0 +1,17 @@
+  #set items can be of any datatype
+  #example :string , int , boolean datatypes
+
+set1 = {"apple","banana","guava"}
+set2 = {1,5,7,9,3}
+set3 = {True,False,False}
+
+print(set1) #{'guava', 'banana', 'apple'}
+print(set2) #{1, 3, 5, 7, 9}
+print(set3) #{False, True}
+
+#one set can contain different datatypes
+set4 = {"bharath",20,True,"male"}
+print(set4) #{True, 'bharath', 20, 'male'}
+
+
+
