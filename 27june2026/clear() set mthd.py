cc23020d3ba2ef12524ee340138a9eb654fdffb4
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-27 07:18:44 +0530

    clear() set method

---
+++ 27june2026/clear() set mthd.py
@@ -0,0 +1,8 @@
+#removing elements from the cars set
+cars = {"BMW","audi","lamborghini","bugati"}
+
+cars.clear()
+print(cars) #set()
+
+#clear()------removes all elements in set
+#syntax: set.clear()
