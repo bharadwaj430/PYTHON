a53f52d92c750f6242247c8630916cca0f3887bb
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-28 14:56:45 +0530

    added dict

---
+++ 28june2026/dict items &length.py
@@ -0,0 +1,22 @@
+dict = {
+"brand" : "benz",
+"model":"GLE 300",
+"year":"1998"
+
+
+}
+print(len(dict)) #3--no of items in dict
+
+my_dict = {
+
+"brand": "Ford", #string
+  "electric": False, #boolean
+  "year": 1964, #integer
+  "colors": ["red", "white", "blue"] #list
+}
+#type()
+
+# dictionaries are defined as objects with the data type 'dict':
+
+print(type(my_dict))
+
