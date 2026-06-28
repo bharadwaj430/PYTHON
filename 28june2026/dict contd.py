c7bce3a85bde31e0a8dee10167d91989588b8969
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-28 14:28:25 +0530

    added dictionaries basics

---
+++ 28june2026/dict contd.py
@@ -0,0 +1,17 @@
+"""
+dictionaries are used to store data values in key:value pairs
+
+A dictionary is a collection which is ordered , changeable and do not allow duplicates
+"""
+
+
+
+
+dict = {
+"brand" : "benz",
+"model":"GLE 300",
+"year":"1998"
+
+
+}
+print(dict)
