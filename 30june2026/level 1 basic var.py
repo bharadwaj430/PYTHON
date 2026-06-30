0a1fd06bf6d2b2ef7c19e2b368e5f02cbc961c92
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-30 15:58:15 +0530

    added level 1 prob in variables

---
+++ 30june2026/level 1 basic var.py
@@ -0,0 +1,45 @@
+#level -1 basic problems
+
+
+
+#2. Multiple Assignment
+#Assign values to three variables in one line
+
+f,g,h = 25.5,35,43
+print(f + g + h) #103.5
+
+
+#3 one value to multiple variables
+k = l = m  = 100
+print(k)
+print(l)
+print(m)
+
+#4  swap variables(without tempo values)
+a = 10
+b = 20
+
+a,b = b,a
+print(f"value of a is {a}.the value of b is {b}")
+
+#here the values of a and b are swapped
+
+#5 swap using a temporary variable
+d = 35
+e = 20
+tempo_variable = d
+d = e
+e = tempo_variable
+print(d) #20
+print(e) #35 
+
+#hence the elements are swapped
+
+
+
+
+
+
+
+
+
