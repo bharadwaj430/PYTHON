dc7955ec8dd2de940d35d2d4704182de67154dc2
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-30 11:43:27 +0530

    added variables concept

---
+++ 30june2026/variables.py
@@ -0,0 +1,43 @@
+#variables are containers for storing data values
+
+x = "chaitanya"
+y = 5
+print(x) #chaitanya
+print(y)#5
+
+#variable type can be changed after declared type is set
+
+x = 4
+x = "madhu"
+print(x) #last declared is considered---madhu
+
+
+#CASTING
+#specifying datatype of variable is by casting
+x = str(3) #3
+y = int(3) #3
+z = float(3) #3.0
+print(x,y,z)
+
+#TYPE ----WE CAN GET DATATYPE of variable with the type() function
+
+m = 25
+n = "rishi"
+print(type(m))#<class 'int'>
+print(type(n))#<class 'str'>
+
+
+#string declaration--- possible in single and double quotes
+x = "srinu"
+#is same
+y = 'satish'
+print(x,y) #srinu satish
+
+
+#variable names are case sensitive
+c = 3
+C = "instagram"
+print(c) 
+print(C)
+#uppercase and lowercase alphabet are stored as different variables
+
