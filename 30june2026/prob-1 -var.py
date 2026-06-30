42062c86da6a67494c12339ef63c35ab4204bac0
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-06-30 12:54:17 +0530

    added variables

---
+++ 30june2026/prob-1 -var.py
@@ -0,0 +1,23 @@
+"""
+1. Variable Declaration
+
+Create variables to store:
+
+Your name
+Age
+Height
+College name
+Whether you are an AI/ML student
+
+Print them in a meaningful sentence.
+"""
+name = "bharadwaj"
+age = 20
+height = 165
+college_name = "bhaskar engineering college"
+branch = "information technology"
+is_aiml_student = True
+
+print(f"My name is{name}.I am {age} years old.My height is {height}.I am currently pursuing B.tech at {college_name}.my target role as AI/ML developer is {is_aiml_student} ")
+
+
