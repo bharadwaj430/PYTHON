0244ea04a93ec18c7439260a89f2053ea093887c
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-07-02 15:44:06 +0530

    added datatypes

---
+++ day102-2jul2026.py/datatypes-1.py
@@ -0,0 +1,46 @@
+"""
+problem 1
+integers
+
+Take an integer and print
+
+1.original number
+2.square
+3.cube
+
+
+"""
+
+
+a = 5
+print("the type of the datatype is:",type(a))
+print("the square of the number is:",a**2)
+print("the cube of the number is:",a**3)
+
+
+
+"""
+problem 2
+
+2.
+
+Take two integers.
+
+Print
+
+sum
+difference
+multiplication
+quotient
+remainder
+floor division
+"""
+
+c = 3
+b = 10
+print("addition of two numbers is:",b+c)
+print("difference of two numbers is:",b-c)
+print("multiplication of two numbers is:",b*c)
+print("quotient of two numbers is:",b/c)
+print("remainder of two numbers is:",b%c)
+print("floor divison of two numbers is:",b//c)
