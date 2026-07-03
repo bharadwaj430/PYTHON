ad830a5e3231d9b186e5ead0e59c7e564b684ae3
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-07-03 19:37:32 +0530

    added problems

---
+++ day103-3jul2026/problems.py
@@ -0,0 +1,32 @@
+#casting
+
+#integers
+x = int(1)
+y =int(2.8)
+z =int("3")
+
+
+print(x)
+print(y)
+print(z)
+
+
+#floats
+
+a= float(1)     
+b = float(2.8)  
+c = float("3")   
+d = float("4.2")
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
+
+
+
+
+
