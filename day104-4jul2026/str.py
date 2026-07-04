b3124ee2fe78a491bde7f4a5d51626fd4e7b2871
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-07-04 22:01:30 +0530

    added strings

---
+++ day104-4jul2026/str.py
@@ -0,0 +1,53 @@
+# #assigning a string to variable
+# a = "hello"
+# print(a)
+# #strings are arrays
+# b = "bharadwaj"
+# print(b[2]) #a
+# print(b[3]) #r
+# print(b[4]) #a
+
+# #looping through string
+# for y in "pomegranate":
+#   print(y) 
+# """
+# output:
+# p
+# o
+# m
+# e
+# g
+# r
+# a
+# n
+# a
+# t
+# e
+# """
+
+# #length of a string
+# c = "engineering college"
+# print(len(c)) #19
+
+#CHECKING A STRING
+
+message = "the best things in life are free"
+print("free" in message)  #True
+
+#with if statement---checking a string
+if "free "in message:
+  print("yes,'free' is present")
+
+#checking if NOT
+text = "stay happy always"
+print("happy" not in text)
+
+#checking not if --- with if statement
+if "happy" not in text:
+  print("no , i am not happy")
+
+
+
+
+
+
