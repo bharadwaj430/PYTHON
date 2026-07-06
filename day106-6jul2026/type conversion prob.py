8db74886aac0fdf72cdd7a02093c26f24b6e720c
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-07-06 21:53:45 +0530

    added casting problems

---
+++ day106-6jul2026/type conversion prob.py
@@ -0,0 +1,58 @@
+"""
+Integer to Float
+
+
+Input integer.
+Convert to float.
+
+Print
+1.value
+2.type
+# """
+a = int(input("enter the integer:")) #5
+ 
+print("the value of float for given input:" , float(a)) #5.0
+print(type(float(a))) #<class 'float'>
+
+
+"""
+Float to integer
+
+
+given input : 45.89
+
+convert to integer
+"""
+
+m = 45.89
+print("the integer value for the given input:" , int(m)) #45
+
+
+# #string to integer
+g = "123"
+print("the value of integer for given input:", int(g)) #123
+
+
+"""
+
+string to float
+
+input is "45.67
+convert into float
+
+multiply by 2
+"""
+
+string = "45.67"
+print("the decimal value for given string is:", float(string)) #45.67
+print("the result after multiplying with 2:", float(string)*2) #91.34
+
+
+
+
+
+
+
+
+
+
