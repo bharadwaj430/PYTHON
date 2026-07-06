12dbb456c51139455223247fbcd5614d87dc93bf
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-07-06 19:11:15 +0530

    added casting and input problems

---
+++ day106-6jul2026/input&casting prob.py
@@ -0,0 +1,51 @@
+# Age after 10 years
+"""
+take age from user
+print age after 10 years
+use casting correctly
+"""
+
+# age = int(input("enter the age:"))
+# print("your age after 10 years is:" , age+10)
+
+
+#sum of two numbers
+"""
+Take input.
+
+Add them.
+
+Observe what happens if casting is removed.
+answer:
+if casting is removed the operation is not performed
+and the inputs are printed normally
+
+
+"""
+# a = int(input("enter the first number:"))
+# b = int(input("enter the second number:"))
+# sum = a+b
+# print(sum)
+
+
+#multiply two decimal numbers
+d = float(input("enter the first decimal number:"))#2.5
+e = float(input("enter the second decimal number:")) #3.6
+print("the product of two numbers",d*e) #9.0
+
+
+#integer division
+f = int(input("enter the first integer:")) #5
+g = int(input("enter the second integer:")) #9
+print("the division of two numbers is:",f/g) #5/9 =0.5555555555555556 
+print("the floor division of two numbers is:",f//g) #the floor division of two numbers is: 0
+print("the remainder of two numbers is",f%g) #the remainder of two numbers is 5
+
+
+
+
+
+
+
+
+ 
