48e83ec0fa9b476f05c84e281afd84fdcd003d33
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-07-07 22:26:17 +0530

    added prob on str

---
+++ day107-7jul2026/string prob.py
@@ -0,0 +1,8 @@
+def reverse_string(text):
+    return text[::-1]
+
+# Example usage:
+word = "python"
+result = reverse_string(word)
+print(f"Original: {word} -> Reversed: {result}")
+# Output: Original: python -> Reversed: nohtyp
