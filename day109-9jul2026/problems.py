b50c2e1772d33987bb49e5338ee37367507aa1bf
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-07-09 20:39:59 +0530

    added slicing

---
+++ day109-9jul2026/problems.py
@@ -0,0 +1,32 @@
+name = "bharadwaj"
+#       012345678
+#       -9-8-7-6-5-4-3-2-1
+print(name)
+
+#length of the string
+print(len(name))#6
+
+#printing first character
+print(name[0]) #b
+print(name[8]) #j
+
+
+#print middle character
+print(name[4])
+
+#reverse a string USING SLICING
+print(name[::-1]) #jawdarahb
+
+#print first three characters
+print(name[0:3]) #bha
+
+#print last 4 characters
+print(name[5:9]) #dwaj
+
+#print every second character
+print(name[::2]) #baawj
+
+
+
+
+
