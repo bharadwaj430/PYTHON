d038b40c7be4024f04496b9a22783c73c6df55ac
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-08-01 21:56:43 +0530

    added tup

---
+++ day124/tuples -2.py
@@ -0,0 +1,16 @@
+#acessing tuple elements
+
+
+tuple = ("Mother Earth","environment","nature")
+print(tuple[1])
+print(tuple[-1])
+print(tuple[1:3])
+print(tuple[:3])
+print(tuple[2:])
+print(tuple[-2:-1])
+
+if "nature" in tuple:
+  print("Yes,'nature' is in this tuple")
+
+
+
