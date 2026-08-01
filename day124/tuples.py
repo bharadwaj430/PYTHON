27f86e720e1d3b8a1900ec7942a207e50821a71a
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-08-01 21:29:46 +0530

    added tuples

---
+++ day124/tuples.py
@@ -0,0 +1,25 @@
+mytuple = ("apple","banana","cherry")
+print(mytuple)
+
+#length of tuple
+print(len(mytuple)) #3
+
+#tuple with one item
+
+tuple1= ("apple",)
+tuple2 = ("cricket")  #not a tuple
+print(type(tuple1)) #<class 'tuple'>
+print(type(tuple2)) #<class 'str'>
+
+#tuples can be of any datatype
+tup1 = ("apple", "banana", "cherry")
+tup2 = (1, 5, 7, 9, 3)
+tup3 = (True, False, False)
+
+
+#one tuple diff datatypes
+tup = ("abc", 34, True, 40, "male")
+
+
+
+
