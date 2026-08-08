ac787c1d0d66e8407c4a05558b123ae897a72d5c
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-08-08 22:17:14 +0530

    added fn

---
+++ fn.py
@@ -0,0 +1,16 @@
+#*args 
+a = int(input())
+b = int(input())
+
+def add(a,b):
+    return a + b
+print(add(a,b))
+
+def subtract(a,b):
+    return a - b
+print(subtract(a,b))
+
+    
+def multiplication(a,b):
+    return a*b
+print(multiplication(a,b))
