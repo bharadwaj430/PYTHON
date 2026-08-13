8a9aef7fca1f298dd1d7f728fb91b43ebdcffcf8
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-08-13 22:37:08 +0530

    added

---
+++ day130/match.py
@@ -0,0 +1,21 @@
+day = 4
+match day:
+  case 1:
+    print("Monday")
+  case 2:
+    print("Tuesday")
+  case 3:
+    print("Wednesday")
+  case 4:
+    print("Thursday")
+  case 5:
+    print("Friday")
+  case 6:
+    print("Saturday")
+  case 7:
+    print("Sunday")
+
+"""
+ output: Thursday
+
+"""
