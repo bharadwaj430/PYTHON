8e2f7d6ffd0f6ce722ee28f3e397137b2094e375
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-08-13 22:52:35 +0530

    added while loop

---
+++ day131/loops.py
@@ -0,0 +1,50 @@
+#python loops are two types 1.for loop 2.while loop
+
+#With the while loop we can execute a set of statements as long as a condition is true
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+"""
+1
+2
+3
+4
+5
+
+
+
+
+"""
+
+#the break statement --- we can stop the loop even if the while condition is true:
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+"""
+1
+2
+3
+"""
+
+#continue statement
+#With the continue statement we can stop the current iteration, and continue with the next:
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+
+#else statement
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
