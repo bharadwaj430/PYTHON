c8c3998be1c4c60a6bed378691287e4b5f74b604
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-08-15 22:42:36 +0530

    added for loops

---
+++ day132/forloop.py
@@ -0,0 +1,22 @@
+#A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+
+#for loops = execute a block of code a fixed number of times.
+#              you can iterate over a range , string , sequence , etc...
+
+
+for x in range(1,11):
+  print(x)
+
+for a in reversed(range(1,11)):
+  print(a)
+
+
+for b in range(2,20,3):
+  print("yes")
+
+
+credit_card = "1245-5678-9012-3456"
+for x in credit_card:
+  print(x)
+
+
