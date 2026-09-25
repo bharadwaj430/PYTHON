9cd1d5ae461d11a56a47a444f3a1334325771877
MARRI BHARADWAJ <bharadwajmarri@gmail.com>
2026-09-25 22:50:39 +0530

    added

---
+++ Range.py/OOPS/intro.py
@@ -0,0 +1,13 @@
+#object-oriented programming
+"""
+OOP allows  to structure code using classes and objects
+1.better organization
+2.better reusability
+
+
+class---defines what obj should look like
+obj---created based on class
+class         objects
+fruit--apple banana mango
+car --   volvo ,audi , toyota
+"""
